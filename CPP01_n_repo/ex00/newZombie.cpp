b048e815af322d5376f90a8a27eb4b4fd245e7bf
Abdellatif Maouni <125679488+Abdellatif231@users.noreply.github.com>
2025-02-22 19:28:52 +0100

    Delete CPP01_n_repo directory

--- CPP01_n_repo/ex00/newZombie.cpp
+++
@@ -1,8 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* newZombie( std::string name )
-{
-	Zombie *h_zombie = new Zombie(name);
-	return h_zombie;
-}
-
--- CPP01_n_repo/ex00/randomChump.cpp
+++
@@ -1,7 +0,0 @@
-#include "Zombie.hpp"
-
-void randomChump(std::string name)
-{
-	Zombie s_zombie(name);
-	s_zombie.annouce();
-}
